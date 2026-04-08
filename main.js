34c4b6ac0666b79761666c72b908ed96266e4341
shanu157 <shanugazie@gmail.com>
2026-04-08 13:40:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ autoUpdater.checkForUpdatesAndNotify();
 autoUpdater.on('update-downloaded', () => {
   autoUpdater.quitAndInstall();
 });
+
+const { autoUpdater } = require('electron-updater');
+
+app.on('ready', () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall();
+});
